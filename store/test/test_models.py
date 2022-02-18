f4a8c9df00fc752b9d2654d94906960eebe53c28
Esat YILMAZ <35402007+codesatyilmaz@users.noreply.github.com>
2022-02-18 23:52:33 +0300

    test create

---
+++ store/test/test_models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from store.models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django', slug='django')
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self) -> None:
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
