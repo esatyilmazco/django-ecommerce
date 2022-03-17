906ce0c74c2380a37f9c1805e63938d694fb9511
Esat YILMAZ <35402007+codesatyilmaz@users.noreply.github.com>
2022-03-17 22:00:46 +0300

    product detail create

--- store/views.py
+++ store/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
@@ -13,3 +12,8 @@ def categories(request):
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store,products,detail.html', {'product': product})
